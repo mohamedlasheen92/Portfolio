c1add38a58f4282213ea60ee9a656513964eaeac
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-05-15 16:51:29 +0300

    ADD toggle sun/moon icon when click

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,30 +1,37 @@
-import { Moon } from "lucide-react";
-import styles from "./header.module.scss";
+import { Moon, Sun } from "lucide-react";
+import "./header.scss";
+import { useState } from "react";
 
 const Header = () => {
-  console.log(styles);
 
+  
+  const [iconMode, setIconMode] = useState(true);
+  const toggleMode = () => setIconMode(!iconMode);
+
+
+  
   return (
     <>
       <header className="header flex items-center justify-between">
         <div></div>
         <nav>
           <ul className="flex space-x-5 py-3 px-7 rounded-full">
-            <li className="font-semibold text-sm">
+            <li className="font-semibold text-sm tracking-widest">
               <a href="/">Home</a>
             </li>
-            <li className="font-semibold text-sm">
+            <li className="font-semibold text-sm tracking-widest">
               <a href="/">Projects</a>
             </li>
-            <li className="font-semibold text-sm">
+            <li className="font-semibold text-sm tracking-widest">
               <a href="/">Contact</a>
             </li>
           </ul>
         </nav>
         <div
-          className={`icon p-2 ${styles.icon} rounded-full cursor-pointer border transition-colors duration-300`}
+          className={`icon p-2 icon rounded-full cursor-pointer border transition-colors duration-300`}
+          onClick={() => toggleMode()}
         >
-          <Moon />
+          {iconMode ? <Moon /> : <Sun className="sun" />}
         </div>
       </header>
     </>
