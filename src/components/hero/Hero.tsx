a8e0f442f14688926bec0edbc075e285fe0f2ed1
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-05-17 16:05:17 +0300

    hero section v1

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,23 @@
+import me from '../../assets/me.png'
+import './hero.scss'
+
 const Hero = () => {
   return (
     <>
-      <section className="hero">
-        <h1 className="title">Hero</h1>
+      <section className="hero flex">
+        <section className="info border border-green-400 grow">
+          <img src={me} alt="me" className="w-32 border rounded-full" />
+          <h1 className="title text-2xl my-3">Full Stack Software Engineer</h1>
+          <p className="description">
+            I’m Mohamed Lasheen, Dedicated and results-driven Full Stack
+            Developer with 2 years of experience in designing, implementing, and
+            maintaining complex software solutions. Adept at both front-end and
+            back-end development, with a passion for creating seamless user
+            experiences. Proven expertise in a variety of programming languages,
+            frameworks, and technologies.
+          </p>
+        </section>
+        <section className="ani border border-blue-500">Animation Part</section>
       </section>
     </>
   );
